d8d5dc068ec7c78add0111c44d5ac428aaacf9c1
Alina Kim <akim66467@gmail.com>
2022-04-21 19:45:58 -0500

    login

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Register />} /> //create a route for each component
+        <Route path='/' element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ app/src/components/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import useForm from './useForm';
+import validate from './validateInfo';
+import Form from './Form.css';
+
+const Login = ({submitForm}) => {
+    const { handleChange, values, handleSubmit, errors } = useForm(
+        submitForm, 
+        validate
+        );
+    
+    return (
+        <div className='form-content-right'>
+            <form className='form' onSubmit={handleSubmit}>
+                <h1>Welcom Back!</h1>
+                <div className='form-inputs'>
+                    <label htmlFor='email'
+                    className='form-label'>
+                    Email Address
+                    </label>
+                        <input 
+                        id='email'
+                        type="email" 
+                        name='email' 
+                        className="form-input"
+                        placeholder='Enter email'
+                        value={values.email}
+                        onChange={handleChange} />
+                        {errors.email && <p>{errors.email}</p>}
+                </div>
+                <div className='form-inputs'>
+                    <label htmlFor='password'
+                    className='form-label'>
+                    Password
+                    </label>
+                        <input 
+                        id='password'
+                        type="password" 
+                        name='password' 
+                        className="form-input"
+                        placeholder='Enter password'
+                        value={values.password}
+                        onChange={handleChange} />
+                        {errors.password && <p>{errors.password}</p>}
+                </div>
+                <button className="form-input-btn" 
+                type='submit'>
+                Sign In
+                </button>
+                <p>Don't have an account?</p>
+                <button className="form-input-btn">
+                Sign Up
+                </button>
+                <a href='/'>Forgot Password?</a>
+            </form>
+        </div>
+    );
+};
+
+
+export default Login;
